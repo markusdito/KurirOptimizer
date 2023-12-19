48754ef7b4d446fd56b33c66dcbdd2091d4c1ced
Markus Dito <markusdito@gmail.com>
2023-12-19 11:47:43 +0700

    adding swing components into utils

---
+++ src/main/java/utils/RoundedButton.java
@@ -0,0 +1,233 @@
+package utils;
+
+/**
+ *
+ * @author Markus
+ */
+
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.geom.Area;
+import java.awt.geom.Rectangle2D;
+import java.awt.geom.RoundRectangle2D;
+import javax.swing.JButton;
+
+public class RoundedButton extends JButton {
+
+    private boolean over;
+    private Color fill;
+    private Color line;
+
+    private Color fillOriginal;
+    private Color fillOver;
+    private Color fillClick;
+    private Color lineOriginal;
+    private Color lineOver;
+    private int strokeWidth;
+    private int roundTopLeft = 0;
+    private int roundTopRight = 0;
+    private int roundBottomLeft = 0;
+    private int roundBottomRight = 0;
+    
+
+    public RoundedButton() {
+        fillOriginal = new Color(52, 152, 219);
+        fillOver = new Color(41, 128, 185);
+        fillClick = new Color(211, 84, 0);
+        lineOriginal = new Color(236, 240, 241);
+        lineOver = new Color(189, 195, 199);
+        strokeWidth = 2;
+        fill = fillOriginal;
+        line = lineOriginal;
+        setOpaque(false);
+        setBorder(null);
+        setFocusPainted(false);
+        setContentAreaFilled(false);
+        setBackground(fillOriginal);
+        setForeground(Color.white);
+        //tambahkan mouse event
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseExited(MouseEvent e) {
+                fill = fillOriginal;
+                line = lineOriginal;
+                over = false;
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                fill = fillOver;
+                line = lineOver;
+                over = true;
+                setCursor(new Cursor(Cursor.HAND_CURSOR));
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                if (over) {
+                    fill = fillOver;
+                    line = lineOver;
+                } else {
+                    fill = fillOriginal;
+                    line = lineOriginal;
+                }
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                fill = fillClick;
+            }
+
+        });
+    }
+
+    public Color getFillOriginal() {
+        return fillOriginal;
+    }
+
+    public void setFillOriginal(Color fillOriginal) {
+        this.fillOriginal = fillOriginal;
+    }
+
+    public Color getFillOver() {
+        return fillOver;
+    }
+
+    public void setFillOver(Color fillOver) {
+        this.fillOver = fillOver;
+    }
+
+    public Color getFillClick() {
+        return fillClick;
+    }
+
+    public void setFillClick(Color fillClick) {
+        this.fillClick = fillClick;
+    }
+
+    public Color getLineOriginal() {
+        return lineOriginal;
+    }
+
+    public void setLineOriginal(Color lineOriginal) {
+        this.lineOriginal = lineOriginal;
+    }
+
+    public Color getLineOver() {
+        return lineOver;
+    }
+
+    public void setLineOver(Color lineOver) {
+        this.lineOver = lineOver;
+    }
+
+    public int getStrokeWidth() {
+        return strokeWidth;
+    }
+
+    public void setStrokeWidth(int strokeWidth) {
+        this.strokeWidth = strokeWidth;
+    }
+
+    public int getRoundTopLeft() {
+        return roundTopLeft;
+    }
+
+    public void setRoundTopLeft(int roundTopLeft) {
+        this.roundTopLeft = roundTopLeft;
+        repaint();
+    }
+
+    public int getRoundTopRight() {
+        return roundTopRight;
+    }
+
+    public void setRoundTopRight(int roundTopRight) {
+        this.roundTopRight = roundTopRight;
+        repaint();
+    }
+
+    public int getRoundBottomLeft() {
+        return roundBottomLeft;
+    }
+
+    public void setRoundBottomLeft(int roundBottomLeft) {
+        this.roundBottomLeft = roundBottomLeft;
+        repaint();
+    }
+
+    public int getRoundBottomRight() {
+        return roundBottomRight;
+    }
+
+    public void setRoundBottomRight(int roundBottomRight) {
+        this.roundBottomRight = roundBottomRight;
+        repaint();
+    }
+
+    @Override
+    protected void paintComponent(Graphics grphcs) {
+        Graphics2D g2 = (Graphics2D) grphcs.create();
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setColor(getBackground());
+        Area area = new Area(createRoundTopLeft());
+        if (roundTopRight > 0) {
+            area.intersect(new Area(createRoundTopRight()));
+        }
+        if (roundBottomLeft > 0) {
+            area.intersect(new Area(createRoundBottomLeft()));
+        }
+        if (roundBottomRight > 0) {
+            area.intersect(new Area(createRoundBottomRight()));
+        }
+        g2.fill(area);
+        g2.dispose();
+        super.paintComponent(grphcs);
+    }
+
+    private Shape createRoundTopLeft() {
+        int width = getWidth();
+        int height = getHeight();
+        int roundX = Math.min(width, roundTopLeft);
+        int roundY = Math.min(height, roundTopLeft);
+        Area area = new Area(new RoundRectangle2D.Double(0, 0, width, height, roundX, roundY));
+        area.add(new Area(new Rectangle2D.Double(roundX / 2, 0, width - roundX / 2, height)));
+        area.add(new Area(new Rectangle2D.Double(0, roundY / 2, width, height - roundY / 2)));
+        return area;
+    }
+
+    private Shape createRoundTopRight() {
+        int width = getWidth();
+        int height = getHeight();
+        int roundX = Math.min(width, roundTopRight);
+        int roundY = Math.min(height, roundTopRight);
+        Area area = new Area(new RoundRectangle2D.Double(0, 0, width, height, roundX, roundY));
+        area.add(new Area(new Rectangle2D.Double(0, 0, width - roundX / 2, height)));
+        area.add(new Area(new Rectangle2D.Double(0, roundY / 2, width, height - roundY / 2)));
+        return area;
+    }
+
+    private Shape createRoundBottomLeft() {
+        int width = getWidth();
+        int height = getHeight();
+        int roundX = Math.min(width, roundBottomLeft);
+        int roundY = Math.min(height, roundBottomLeft);
+        Area area = new Area(new RoundRectangle2D.Double(0, 0, width, height, roundX, roundY));
+        area.add(new Area(new Rectangle2D.Double(roundX / 2, 0, width - roundX / 2, height)));
+        area.add(new Area(new Rectangle2D.Double(0, 0, width, height - roundY / 2)));
+        return area;
+    }
+
+    private Shape createRoundBottomRight() {
+        int width = getWidth();
+        int height = getHeight();
+        int roundX = Math.min(width, roundBottomRight);
+        int roundY = Math.min(height, roundBottomRight);
+        Area area = new Area(new RoundRectangle2D.Double(0, 0, width, height, roundX, roundY));
+        area.add(new Area(new Rectangle2D.Double(0, 0, width - roundX / 2, height)));
+        area.add(new Area(new Rectangle2D.Double(0, 0, width, height - roundY / 2)));
+        return area;
+    }
+
+}
